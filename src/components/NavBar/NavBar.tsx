a8b329b097e3c8ee8ceae80f06dbcafc6e4d3370
Bryan Hernandez <herna536@uwm.edu>
2023-09-19 02:13:57 -0500

    NavBar visual update, Login structure added, png's added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <>
-      <Link to="trailsearch">Find a Trail</Link>
-      <span> </span>
-      <Link to="mytrails">My Trails</Link>
-      <span> </span>
-      <Link to="community">Community</Link>
-      <span> </span>
-      <Link to="login"> Login</Link>
-    </>
+
+
+<Navbar bg="dark" expand="lg" variant="dark" className="navbar-custom">
+<Navbar.Brand href="/"><img
+              src="/XCSInverted.png"
+              width="250"
+              height="72"
+              alt= "logo"
+            /></Navbar.Brand>
+<Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar.Collapse id="basic-navbar-nav">
+  <Nav className="ml-auto">
+    <Nav.Link href="trailsearch">Find a Trail</Nav.Link>
+    <Nav.Link href="mytrails">My Trails</Nav.Link>
+    <Nav.Link href="community">Community</Nav.Link>
+    <Nav.Link href="login"> Login</Nav.Link>
+  </Nav>
+</Navbar.Collapse>
+</Navbar>
   );
 };
 
