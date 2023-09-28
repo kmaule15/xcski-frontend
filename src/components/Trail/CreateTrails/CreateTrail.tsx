0d3561ebc1fa062bd47d921f2332121f09067e0b
kmaule15 <kaylamaule15@gmail.com>
2023-09-27 22:05:06 -0500

    form clears values after successfully submitted

--- src/components/Trail/CreateTrails/CreateTrail.tsx
+++ src/components/Trail/CreateTrails/CreateTrail.tsx
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
 
 const CreateTrail = () => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [location, setLocation] = useState("");
-  const [difficulty, setDifficulty] = useState("");
-  const [length, setLength] = useState("");
-  const [estimatedTime, setEstimatedTime] = useState("");
-  // const [typesAllowed, setTypesAllowed] = useState([""]);
-  let typesAllowed = ["classic"];
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [location, setLocation] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<string>("");
+  const [length, setLength] = useState<number | null>(null);
+  const [estimatedTime, setEstimatedTime] = useState<number | null>(null);
+  const [typesAllowed, setTypesAllowed] = useState<string[]>([]);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  const clearForm = () => {
+    setName("");
+    setDescription("");
+    setLocation("");
+    setDifficulty("");
+    setLength(null);
+    setEstimatedTime(null);
+    setTypesAllowed([]);
+  };
+
+  const handleTypesAllowedChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = event.target.value;
+
+    // add value to array if it's checked
+    // otherise remove value
+    if (event.target.checked) {
+      setTypesAllowed((prevTypesAllowed) => [...prevTypesAllowed, value]);
+    } else {
+      setTypesAllowed((prevTypesAllowed) =>
+        prevTypesAllowed.filter((item) => item !== value)
+      );
+    }
+  };
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -32,88 +59,128 @@ const CreateTrail = () => {
       });
 
       if (response.ok) {
-        console.log("Sucess");
+        console.log("Trail successfully created!");
+        setIsSuccess(true);
+        clearForm();
+        setTimeout(() => setIsSuccess(false), 4000);
       } else {
-        console.error("Fail");
+        console.error("Trail creation failed.");
       }
     } catch (error) {
-      console.error(error);
+      console.error("An error occured:", error);
     }
   };
 
   return (
     <>
-      <h1>Create a Trail</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <label>
-          Description
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <label>
-          Location
-          <input
-            type="text"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <label>
-          Difficulty
-          <select
-            value={difficulty}
-            onChange={(e) => setDifficulty(e.target.value)}
-          >
-            <option value="">--Select--</option>
-            <option value="easy">easy</option>
-            <option value="medium">medium</option>
-            <option value="difficult">difficult</option>
-          </select>
-        </label>
-        <br></br>
-        <label>
-          Length (in miles)
-          <input
-            type="text"
-            value={length}
-            onChange={(e) => setLength(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <label>
-          Estimated Time (in minutes)
-          <input
-            type="text"
-            value={estimatedTime}
-            onChange={(e) => setEstimatedTime(e.target.value)}
-          />
-        </label>
-        <br></br>
-        {/* <label>
-          Types Allowed
-          <input
-            type="text"
-            value={typesAllowed}
-            onChange={(e) => setTypesAllowed(e.target.value)}
-          />
-        </label> */}
-        <br />
-        <button type="submit">Create Trail</button>
-      </form>
+      <h1 className="text-center mb-4">Create a Trail</h1>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Description</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Location</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Difficulty</label>
+                <select
+                  className="form-select"
+                  value={difficulty}
+                  onChange={(e) => setDifficulty(e.target.value)}
+                >
+                  <option value="">--Select--</option>
+                  <option value="Easy">Easy</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Difficult">Difficult</option>
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Length (in miles)</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={length ?? ""}
+                  onChange={(e) => setLength(Number(e.target.value))}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">
+                  Estimated Time (in minutes)
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={estimatedTime ?? ""}
+                  onChange={(e) => setEstimatedTime(Number(e.target.value))}
+                />
+              </div>
+
+              <p>Types of Skiing Allowed</p>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value="Classic"
+                  onChange={handleTypesAllowedChange}
+                  id="classicCheck"
+                />
+                <label className="form-check-label" htmlFor="classicCheck">
+                  Classic
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value="Skate"
+                  onChange={handleTypesAllowedChange}
+                  id="skateCheck"
+                />
+                <label className="form-check-label" htmlFor="skateCheck">
+                  Skate
+                </label>
+              </div>
+              {isSuccess && (
+                <div className="alert alert-success">
+                  Trail successfully created!
+                </div>
+              )}
+              <button type="submit" className="btn btn-primary mt-3">
+                Create Trail
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
