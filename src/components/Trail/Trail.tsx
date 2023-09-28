385fd6abbd3ac77b54c43e12cee698a87ba2030b
Chase Andreasen <andreas7@uwm.edu>
2023-09-28 18:25:23 -0500

    ok now just need an api key

--- src/components/Trail/Trail.tsx
+++ src/components/Trail/Trail.tsx
@@ -1,6 +1,6 @@
-import { GoogleMap, Marker } from '@react-google-maps/api';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
-function MapComponent() {
+const MapComponent = () => {
   const mapContainerStyle = {
     width: '100%',
     height: '100vh',
@@ -12,15 +12,17 @@ function MapComponent() {
   };
 
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      center={center}
-      zoom={12}
-    >
-      <Marker position={center} />
-    </GoogleMap>
+    <LoadScript googleMapsApiKey="YOUR_API_KEY">
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={12}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
   );
-}
+};
 
 const Trail = () => {
   return <MapComponent />;
