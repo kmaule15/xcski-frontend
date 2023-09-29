4732b2c86cea03a160e1ae40da040635334108c8
Chase Andreasen <andreas7@uwm.edu>
2023-09-28 19:49:49 -0500

    added the api key to .env

--- src/components/Trail/Trail.tsx
+++ src/components/Trail/Trail.tsx
@@ -1,6 +1,13 @@
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
 const MapComponent = () => {
+
+  const apiKey = process.env.REACT_APP_Google_Maps_API_KEY;
+
+  if (!apiKey) {
+    throw new Error('API key is missing. Please check your .env file.');
+  }
+
   const mapContainerStyle = {
     width: '100%',
     height: '100vh',
@@ -12,7 +19,7 @@ const MapComponent = () => {
   };
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyDmP8oo5c41zTyGihTAH0M_dcqxJ76yC7Q">
+    <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         center={center}
