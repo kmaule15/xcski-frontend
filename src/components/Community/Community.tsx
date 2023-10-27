62e7bc05e45c9a1258bc69be6681fec2820a26cb
Bryan Hernandez <herna536@uwm.edu>
2023-10-27 00:41:52 -0500

    Current state

--- src/components/Community/Community.tsx
+++ src/components/Community/Community.tsx
@@ -1,8 +1,28 @@
 import PostComments from "./PostComments";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Community.css';
 
 
 const Community = () => {
-  return <PostComments postId={1}/>;
+  return ( 
+    <div className="container">
+    <div className="row">
+      <div className="col-sm-2 rounded-left">
+        {/* Left Column */}
+        <p>Left Column</p>
+      </div>
+      <div className="col-md-7 rounded-middle">
+        {/* Middle Rectangle */}
+        <p>Middle Rectangle</p>
+      </div>
+      <div className="col-sm-2 rounded-right right-column">
+        {/* Right Column */}
+        <p>Right Column</p>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Community;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,13 @@
 import SearchBarComponent from "../SearchBar/SearchBarComponent";
-
+import BackgroundSlideshow from './BGSlideshow';
 const Home = () => {
   return (
-    <>
-      <p>This will contain the homepage content</p>
-    </>
+    <div>
+       <BackgroundSlideshow />
+    <div className="container d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <SearchBarComponent></SearchBarComponent>
+    </div>
+    </div>
   );
   
 };
