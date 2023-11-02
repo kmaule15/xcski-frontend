03c8f5e359827620b17c0fd41302fe1e0409f7ea
Bryan Hernandez <herna536@uwm.edu>
2023-11-02 01:51:34 -0500

    Check apiKey use

--- src/components/Community/Community.tsx
+++ src/components/Community/Community.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PostsList from "./Posts/PostList";
 import './Community.css';
-
+import WeatherWidget from "../Trail/WeatherWidget/WeatherWidget";
 
 const Community = () => {
   return (
@@ -15,7 +15,8 @@ const Community = () => {
         {/* Middle Rectangle */}
         <div>
       <PostsList />
-      
+      {/* weather widget here just for showing use */}
+      <WeatherWidget {...{lat: 1, lng: 2}}></WeatherWidget>
     </div>
       </div>
       <div className="col-sm-2 rounded-right right-column">
