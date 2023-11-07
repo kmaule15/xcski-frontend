b979486aa47d22a0a10375410f706c8cbc832111
Chase Andreasen <andreas7@uwm.edu>
2023-11-06 19:40:17 -0600

    playing with overlays

--- src/components/Trail/SearchTrails/TrailSearch.tsx
+++ src/components/Trail/SearchTrails/TrailSearch.tsx
@@ -8,18 +8,30 @@ const TrailSearch = () => {
     setSearchTerm(event.target.value);
   };
 
+  // This is a placeholder for the list of trails from the database.
+  // Replace this with the actual data.
+  const trails = ['Trail 1', 'Trail 2', 'Trail 3'];
+
   return (
-    <>
-      <input
-        type="text"
-        placeholder="Search trails..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-      <p>This is the trail search page</p>
-      <p>A display of trails</p>
-      <Trail />
-    </>
+    <div style={{ width: '100%', position: 'relative', zIndex: 2 }}>
+      <div style={{ width: '50%', position: 'absolute', top: '33%', zIndex: 3, display: 'flex', justifyContent: 'center' }}>
+        <input
+          type="text"
+          placeholder="Search trails..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          style={{ width: '80%', textAlign: 'center' }}
+        />
+      </div>
+      <div style={{ width: '25%', height: '100%', position: 'absolute', left: 0, top: '33%', zIndex: 3, backdropFilter: 'blur(10px)', backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+        {trails.map((trail, index) => (
+          <p key={index}>{trail}</p>
+        ))}
+      </div>
+      <div style={{ width: '100%', height: '100%', position: 'absolute', right: 0, zIndex: 1 }}>
+        <Trail />
+      </div>
+    </div>
   );
 };
 
