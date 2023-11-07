3530dc24541de9ca3ec8954452d011fa50b8789e
Chase Andreasen <andreas7@uwm.edu>
2023-11-06 19:16:15 -0600

    started search layout

--- src/components/Trail/SearchTrails/TrailSearch.tsx
+++ src/components/Trail/SearchTrails/TrailSearch.tsx
@@ -1,8 +1,21 @@
+import React, { useState } from 'react';
 import Trail from "../Trail";
 
 const TrailSearch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <>
+      <input
+        type="text"
+        placeholder="Search trails..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
       <p>This is the trail search page</p>
       <p>A display of trails</p>
       <Trail />
