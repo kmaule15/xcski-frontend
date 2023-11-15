5edbd51a9ec708bd002a2522f52dc08694e9e216
Chase Andreasen <andreas7@uwm.edu>
2023-11-14 20:13:15 -0600

    removed geocoding for now think i wanna rework this

--- src/components/Trail/SearchTrails/TrailSearch.tsx
+++ src/components/Trail/SearchTrails/TrailSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, InputGroup, FormControl, Card, ButtonGroup, Button } from 'react-bootstrap';
 import MapComponent, { useTrails } from "../Trail";
 
@@ -6,52 +6,13 @@ const TrailSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const { trails, isLoading, error } = useTrails();
   const [sortField, setSortField] = useState('name');
-  const [userLocation, setUserLocation] = useState<{ latitude: number; longitude: number } | null>(null);
-  const [address, setAddress] = useState('');
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAddress(event.target.value);
-  };
-
-  const handleAddressSubmit = () => {
-    // Use a geocoding service to convert the address to coordinates
-    // This is just a placeholder, replace it with actual geocoding logic
-    const geocodedLocation = geocodeAddress(address);
-    if (geocodedLocation) {
-      setUserLocation(geocodedLocation);
-    } else {
-      alert('Invalid address. Please try again.');
-    }
-  };
-
-  const getDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-    const R = 6371; // Radius of the earth in km
-    const dLat = deg2rad(lat2 - lat1);
-    const dLon = deg2rad(lon2 - lon1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c; // Distance in km
-    return d * 0.621371; // Convert distance to miles
-  }
-
-  const deg2rad = (deg: number) => {
-    return deg * (Math.PI / 180);
-  }
-
   const sortedTrails = [...trails].sort((a, b) => {
-    if (userLocation) {
-      const distanceA = getDistance(userLocation.latitude, userLocation.longitude, a.latitude, a.longitude);
-      const distanceB = getDistance(userLocation.latitude, userLocation.longitude, b.latitude, b.longitude);
-      return distanceA - distanceB;
-    }
-    return 0;
+    return a[sortField] > b[sortField] ? 1 : -1;
   });
 
   if (isLoading) {
@@ -72,20 +33,11 @@ const TrailSearch = () => {
           value={searchTerm}
           onChange={handleSearchChange}
         />
-        <FormControl
-          placeholder="Enter your address..."
-          aria-label="Enter your address"
-          aria-describedby="basic-addon2"
-          value={address}
-          onChange={handleAddressChange}
-        />
-        <Button onClick={handleAddressSubmit}>Submit</Button>
       </InputGroup>
       <ButtonGroup aria-label="Sort trails">
         <Button variant="secondary" onClick={() => setSortField('name')}>Sort by Name</Button>
         <Button variant="secondary" onClick={() => setSortField('difficulty')}>Sort by Difficulty</Button>
         <Button variant="secondary" onClick={() => setSortField('length')}>Sort by Length</Button>
-        <Button variant="secondary" onClick={() => setSortField('distance')}>Sort by Distance</Button> {/* Add a button for sorting by distance */}
       </ButtonGroup>
       <Row style={{ flex: 1 }}>
         <Col md={3} style={{ overflowY: 'auto', paddingRight: 0 }}>
@@ -97,17 +49,14 @@ const TrailSearch = () => {
                   <p>{trail.description}</p>
                   <p>Location: {trail.location}</p>
                   <p>Difficulty: {trail.difficulty}</p>
-                  <p>Length: {trail.length} miles</p>
-                  <p>Estimated Time: {trail.estimatedTime} hours</p>
-                  <p>Types Allowed: {trail.typesAllowed.join(', ')}</p>
-                  {userLocation && <p>Distance: {getDistance(userLocation.latitude, userLocation.longitude, trail.latitude, trail.longitude)} miles</p>}
+                  <p>Length: {trail.length}</p>
                 </Card.Text>
               </Card.Body>
             </Card>
           ))}
         </Col>
         <Col md={9}>
-          <MapComponent trails={sortedTrails} userLocation={userLocation} />
+          <MapComponent />
         </Col>
       </Row>
     </Container>
