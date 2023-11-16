2a91b364481960b5073864d09157a5b336427fb8
vaishnavi-manikonda <66915964+vaishnavi-manikonda@users.noreply.github.com>
2023-11-16 16:15:40 -0600

    Revert "Searchbar working!"
    This reverts commit 8458f45e637bdd6fc0ea3e206f009672d9aed55c.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,65 +1,11 @@
-import { useEffect, useState } from "react";
-import SearchBar from "../SearchBar/SearchBar";
+import SearchBarComponent from "../SearchBar/SearchBarComponent";
 import BackgroundSlideshow from './BGSlideshow';
 const Home = () => {
-  //start searchbar stuff
-  const [trailName, setTrailName] = useState<string>("");
-  const [trailId, setTrailId] = useState<number>(-1);
-  useEffect(()=>{onLoad();}, []);
-  const [results, setResults] = useState<{ name: string; location: string; id: number }[]>();
- const [trails, setTrails] = useState<{ name: string; location: string; id: number }[]>();
- const [selectedTrail, setSelectedTrail] = useState<{
-    name: string;
-    location: string;
-    id: number;
-  }>();
-  const onLoad = async () => {
-    console.log("Trails queried!");
-    try {
-      const response = await fetch("http://localhost:3000/trails", {
-        method: "Get",
-      }).then(response => response.json())
-      .then(data => setTrails(data));
-
-    } catch (error) {
-      //console.error("An error occured:", error);
-    }
-  };
-  type changeHandler = React.ChangeEventHandler<HTMLInputElement>;
-  const handleChange: changeHandler = (e) => {
-    const { target } = e;
-    if (!target.value.trim()) return setResults([]);
-    //improve filtering later
-    var targetValue = target.value.toLowerCase()
-    const filteredValue = trails && trails.filter((trail: { name: string; location: string; id: number;}) =>
-      trail.name.toLowerCase().includes(targetValue) || trail.location.toLowerCase().includes(targetValue)
-    );
-    setResults(filteredValue);
-  };
-
-  const handleSelect = (trail: any) => {
-    setSelectedTrail(trail);
-    setTrailName(trail?.name);
-    setTrailId(trail?.id);
-  };
-  //end searchbar stuff
-
   return (
     <div>
        <BackgroundSlideshow />
-    <div className="container justify-content-center align-items-center" style={{ height: '100vh' }}>
-    <SearchBar
-                results={results}
-                value={trailName}//change to trailname maybe
-                renderItem={(trail: any) => 
-                    <div>
-                        <p>{trail.name}</p>
-                        <p>{trail.location}</p>
-                   </div>
-                }
-                onChange={handleChange}
-                onSelect={handleSelect}
-               />
+    <div className="container d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <SearchBarComponent></SearchBarComponent>
     </div>
     </div>
   );
