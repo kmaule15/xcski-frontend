b30adb171591967baf749d71e5cfb89e83fc47af
kmaule15 <kaylamaule15@gmail.com>
2023-11-29 21:21:39 -0600

    just kidding backend not frontend

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = () => {
     const fetchTrails = async () => {
       try {
         const response = await axios.get<Trail[]>(
-          `${process.env.REACT_APP_FRONTEND_URL}/trails/grooming-data`
+          `${process.env.REACT_APP_BACKEND_URL}/trails/grooming-data`
         );
         setTrails(response.data);
       } catch (error) {
